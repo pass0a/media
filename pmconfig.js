f369fb75974b56261873931b73e95dc97e425cb0
liuwenjun <liuwenjun@hangsheng.com.cn>
2019-09-27 15:47:28 +0800

    fixed npm run pm-msvc

--- pmconfig.js
+++ pmconfig.js
@@ -1,6 +1,6 @@
 module.exports = [
 	{
 		name: 'libui',
-		msvc: 'https://github.com/pass0a/libui/releases/download/v1.0.0/libui-msvc-v1.0.0.tar.gz'
+		msvc: 'https://github.com/pass0a/libui/releases/download/v1.0.1/libui-msvc-v1.0.1.tar.gz'
 	}
 ];
