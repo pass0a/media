29640d23bb2307344475aa729e4f588f73b3e584
liuwenjun <liuwenjun@hangsheng.com.cn>
2019-10-24 11:31:41 +0800

    fixed build step

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,18 @@ const path = require('path');
 const fs = require('fs-extra');
 let opt_dist = 'dist';
 let opt_watch = false;
-fs
-	.copy('src/sn.js', path.resolve(__dirname, `${distfolder}/sn.js`))
-	.then(() => {
-		console.log('copy run.js success!');
-	})
-	.catch((err) => {
-		console.error(err);
-	});
-module.exports = (env) => {
+if (process.env.opt == 'build') {
+	fs
+		.copy('src/sn.js', path.resolve(__dirname, `${opt_dist}/sn.js`))
+		.then(() => {
+			console.log('copy run.js success!');
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+}
+
+module.exports = () => {
 	if (process.env.opt == 'dev') {
 		opt_watch = true;
 		opt_dist = 'dist';
